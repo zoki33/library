d2a8c0c19d7772576bd05a0eead558a55cbe30ff
zoki33 <zoxwow94@hotmail.com>
2023-12-03 15:29:23 +0100

    Add display books but needs refining

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const myLibrary = [];
+let myLibrary = [];
 
 function Book(name, author, pages, read) {
   this.name = name;
@@ -10,11 +10,29 @@ function Book(name, author, pages, read) {
 const form = document.getElementById("input-form");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  let name = document.getElementById("book-title").value;
-  let author = document.getElementById("book-author").value;
-  let pages = document.getElementById("book-pages").value;
-  let read = document.getElementById("book-read").checked;
+  let name = document.getElementById("book_title").value;
+  let author = document.getElementById("book_author").value;
+  let pages = document.getElementById("book_pages").value;
+  let read = document.getElementById("book_read").checked;
 
   let book = new Book(name, author, pages, read);
-  myLibrary.push(book);
+  myLibrary = [...myLibrary, book];
+  form.reset();
 });
+
+const openBtn = document.getElementById("btn-library");
+
+openBtn.addEventListener("click", showBooks);
+
+function showBooks() {
+  let container = document.getElementById("book-container");
+
+  for (let i = 0; i < myLibrary.length; i++) {
+    let book = document.createElement("div");
+    let info = document.createElement("p");
+    info.innerHTML = `Book name: ${myLibrary[i].name}`;
+    book.appendChild(info);
+    container.appendChild(book);
+  }
+  window.location = "./homepage.html";
+}
